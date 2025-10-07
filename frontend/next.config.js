56060cfe34b67fd32c4bdd2d6a09ea1ad8caf49b
Rynawkin <ucarere@gmail.com>
2025-10-07 12:35:26 +0300

    Add Vercel proxy for backend API

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,6 +4,14 @@ const nextConfig = {
   images: {
     domains: ['localhost'],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://139.59.133.81/api/:path*',
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
