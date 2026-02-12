fe9620998cbe7ccb417360e76c67a0613434f438
ucarere <ucarere@gmail.com>
2026-02-12 17:17:05 +0300

    Fix VAT rate loading for customer order approvals

--- backend/src/services/order.service.ts
+++ backend/src/services/order.service.ts
@@ -385,6 +385,7 @@ class OrderService {
                 name: true,
                 mikroCode: true,
                 imageUrl: true,
+                vatRate: true,
               },
             },
           },
